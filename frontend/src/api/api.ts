3175d17600c2bc0b601b4cccbee8eaa6e3d2d508
nandini <nidadavolunandini@gmail.com>
2026-02-25 23:57:11 +0530

    Add

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,10 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:3000",
+    import.meta.env.VITE_API_URL
 });
-console.log(import.meta.env.VITE_API_URL)
+console.log(import.meta.env.VITE_API_URL);
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("authToken");
 
